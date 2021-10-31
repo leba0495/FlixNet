46706efcdb425a2d357d74c6ab87d7a19a558b50
LuisBcode <ab.luis04@gmail.com>
2021-10-31 15:42:38 -0400

    Newest changes

--- FlixNet/Controller/MovieGridViewController.swift
+++ FlixNet/Controller/MovieGridViewController.swift
@@ -43,13 +43,14 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
                     print(error.localizedDescription)
              } else if let data = data {
                     let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
                 self.movies = dataDictionary["results"] as! [[String : Any]]
                 
                 self.collectionView.reloadData()
                     // TODO: Get the array of movies
                     // TODO: Store the movies in a property to use elsewhere
                     // TODO: Reload your table view data
-                //print(self.movies)
+                print(self.movies)
              }
         }
         task.resume()
